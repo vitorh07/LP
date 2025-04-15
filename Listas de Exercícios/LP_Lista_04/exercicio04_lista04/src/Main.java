1552792d7d047816c65ec8e892f276264fddd349
vitorh07 <vitor07devsenai@gmail.com>
2025-04-15 12:52:40 -0300

    Lista 4 completa

--- Listas de Exercícios/LP_Lista_04/exercicio04_lista04/src/Main.java
+++ Listas de Exercícios/LP_Lista_04/exercicio04_lista04/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Tabela de Conversão de Celsius para Fahrenheit");
+        System.out.println("---------------------------------------------");
+        System.out.println("Celsius\tFahrenheit");
+
+        for (int celsius = -80; celsius <= 80; celsius += 10) {
+            double fahrenheit = (9.0 / 5.0) * celsius + 32;
+
+            System.out.printf("%3d\t%.1f\n", celsius, fahrenheit);
+        }
     }
 }
--- Listas de Exercícios/LP_Lista_04/exercicio10_lista04/src/Main.java
+++ Listas de Exercícios/LP_Lista_04/exercicio10_lista04/src/Main.java
@@ -6,5 +6,21 @@ public class Main {
         System.out.println("Sequência de Fibonacci");
         System.out.print("Digite um número: ");
         int limite = entrada.nextInt();
+        int a = 0;
+        int b = 1;
+        System.out.println(a);
+        if (limite > 0) {
+            System.out.println(b);
+        }
+        while(true){
+            int proximo = a+b;
+            if(proximo > limite){
+                break;
+            }
+            System.out.println(proximo);
+            a = b;
+            b = proximo;
+        }
+        entrada.close();
     }
 }
