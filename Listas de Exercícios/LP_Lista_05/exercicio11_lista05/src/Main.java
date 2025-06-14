4ffadf3a7d0c1ed328c7bd2a2e3736f4bfb21d8d
vitorh07 <vitor07devsenai@gmail.com>
2025-06-13 23:06:57 -0300

    Lista 05 completa

--- Listas de Exercícios/LP_Lista_05/exercicio11_lista05/src/Main.java
+++ Listas de Exercícios/LP_Lista_05/exercicio11_lista05/src/Main.java
@@ -1,9 +1,61 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        int[][] matriz = {{1, 2},
-                {1, 2},
-                {1, 2}};
-        int[][] matriz2 = {{1, 2},
-                {1, 2},};
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe linhas e colunas da 1ª matriz (li1 col1): ");
+        int li1 = sc.nextInt();
+        int col1 = sc.nextInt();
+
+        System.out.print("Informe linhas e colunas da 2ª matriz (li2 col2): ");
+        int li2 = sc.nextInt();
+        int col2 = sc.nextInt();
+
+        if (col1 != li2) {
+            System.out.println("\nNão é possível multiplicar: "
+                    + "colunas da 1ª matriz (" + col1 + ") "
+                    + "≠ linhas da 2ª matriz (" + li2 + ").");
+            return;
+        }
+
+        int[][] matriz1 = new int[li1][col1];
+        int[][] matriz2 = new int[li2][col2];
+
+        System.out.println("\nDigite os elementos da 1ª matriz (" + li1 + "x" + col1 + "):");
+        for (int i = 0; i < li1; i++) {
+            for (int j = 0; j < col1; j++) {
+                matriz1[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("\nDigite os elementos da 2ª matriz (" + li2 + "x" + col2 + "):");
+        for (int i = 0; i < li2; i++) {
+            for (int j = 0; j < col2; j++) {
+                matriz2[i][j] = sc.nextInt();
+            }
+        }
+
+        int[][] matrizProduto = new int[li1][col2];
+        for (int i = 0; i < li1; i++) {
+            for (int j = 0; j < col2; j++) {
+                int soma = 0;
+                for (int k = 0; k < col1; k++) {
+                    soma += matriz1[i][k] * matriz2[k][j];
+                }
+                matrizProduto[i][j] = soma;
+            }
+        }
+
+        System.out.println("\nMatriz produto (" + li1 + "x" + col2 + "):");
+        for (int i = 0; i < li1; i++) {
+            for (int j = 0; j < col2; j++) {
+                System.out.printf("%d ", matrizProduto[i][j]);
+            }
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
--- Listas de Exercícios/LP_Lista_05/exercicio12_lista05/src/Main.java
+++ Listas de Exercícios/LP_Lista_05/exercicio12_lista05/src/Main.java
@@ -1,7 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        int[][] matriz = {{1, 2, 3},
-                         {1, 2, 3},
-                         {1, 2, 3}};
+        int[][] matriz = {
+                {2, 3, 1},
+                {4, 1, 5},
+                {7, 2, 6}
+        };
+
+        int somaPrincipal = 0;
+        int somaSecundaria = 0;
+
+        for (int i = 0; i < 3; i++) {
+            somaPrincipal += matriz[0][i % 3] * matriz[1][(i + 1) % 3] * matriz[2][(i + 2) % 3];
+        }
+
+        for (int i = 0; i < 3; i++) {
+            somaSecundaria += matriz[0][(2 - i + 3) % 3] * matriz[1][(1 - i + 3) % 3] * matriz[2][(3 - i) % 3];
+        }
+
+        int determinante = somaPrincipal - somaSecundaria;
+
+        System.out.println("Determinante da matriz 3x3: " + determinante);
     }
 }
